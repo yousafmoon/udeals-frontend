3973c0c458dbbeb8feb00e2b7cc99260f9167ff7
yousafmoon <yousaf.moon@gmail.com>
2025-07-01 17:39:44 +0500

    fix: add dynamic const in search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState, useRef } from "react";
 import DealListings from "@/components/DealListings";
