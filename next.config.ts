d64f81e33ddb2b2d22e0ae5062b6d6243f362bf9
yousafmoon <yousaf.moon@gmail.com>
2025-07-01 17:17:59 +0500

    fix: trying again temporary disable elint

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig: import('next').NextConfig = {
-   eslint: {
+const nextConfig = {
+  eslint: {
     ignoreDuringBuilds: process.env.SKIP_LINT === 'true',
   },
   images: {
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'ujz.cuf.temporary.site',
-    },
-   
-  ],
-},
-
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ujz.cuf.temporary.site',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
